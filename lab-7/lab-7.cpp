7874df63d01fd833a73076c6b98ef235af923f20
Ihnatenko Vitalii <Ihnatenko Vitalii@DESKTOP-DGTQ2P4>
2023-01-12 11:36:14 +0200

    Добавьте файлы проекта.

---
+++ lab-7/lab-7.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+void foo()
+{
+    cout << "I function called me" << endl;
+}
+int Sum(int a, int b)
+{
+    int result;
+    result = a + b;
+    return result;
+
+    return a + b;
+}
+//Виклик//
+void main()
+{
+    foo();
+    int c = Sum(23, 76);
+    cout << c << endl;
+    int x, y;
+    cout << "Enter the value x" << endl;
+    cin >> x;
+    cout << "Enter the value y" << endl;
+    cin >> y;
+    cout << Sum(x, y) << endl;
+}
